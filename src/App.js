4c9102e3e4b9249c6af36e3dc95c44d3e0b22af6
bramuel <jorammanoah1@gmail.com>
2021-02-17 12:05:17 -0800

    Dependancies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import './bootstrap/css/bootstrap.min.css'
+import Dashboard from "./Dashboard";
+import Nav from "./Nav";
+import About from "./About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Details from './Details'
+import Header from './Components/Header'
+import Main from './Components/Main'
+import Section from './Components/Section'
+import NavBar from './Components/NavBar'
+import Courses from './Components/Courses'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+      <NavBar />
+      <Courses />
+
+      </>
+
+    );
+  }
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
 
 export default App;
